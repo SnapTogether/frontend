e7fbd786ade0194b3a8cf9a7921cdb0322ed63ac
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2025-03-06 10:10:20 +0100

    fix build error

--- snaptogether/src/app/page.tsx
+++ snaptogether/src/app/page.tsx
@@ -5,8 +5,8 @@ import Button from "@/components/Button/Button";
 import Navbar from "@/components/Navbar/Navbar";
 import { PartyPopper } from "lucide-react";
 import PageTransition from "@/components/PageTransition/PageTransition";
-import dynamic from "next/dynamic";
-const FireworksBackground = dynamic(() => import("@/components/ConfettiBackground/FireworksBackground"), { ssr: false });
+// import dynamic from "next/dynamic";
+// const FireworksBackground = dynamic(() => import("@/components/ConfettiBackground/FireworksBackground"), { ssr: false });
 
 export default function Home() {
   const router = useRouter();
