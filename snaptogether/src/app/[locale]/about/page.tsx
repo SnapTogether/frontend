1318932ac6e36648b367993fc8dad15725f22765
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2025-03-31 17:52:06 +0200

    about page

--- snaptogether/src/app/[locale]/about/page.tsx
+++ snaptogether/src/app/[locale]/about/page.tsx
@@ -13,6 +13,8 @@ import { useTranslations } from "next-intl";
 import CircleImage from "../../../../public/about/about-avatar.png";
 import CircleImage2 from "../../../../public/about/scan-qr.png";
 import CircleImage3 from "../../../../public/about/about-avatar3.png";
+import Mockup from "../../../../public/logo/mockup.png";
+
 
 type Section = {
     title: string;
@@ -71,12 +73,42 @@ export default function AboutPage() {
   const steps = t.raw("guidanceSteps");
 
   return (
-    <div className="relative min-h-screen bg-gradient-to-br from-[#1c1c1c] to-[#2e2e2e] text-white px-4 py-8">
+    <div className="relative min-h-screen bg-gradient-to-br from-[#1c1c1c] to-[#2e2e2e] text-white pb-8">
       <Navbar />
 
-      <div className="max-w-3xl mx-auto pt-[10vh] text-center flex flex-col items-center justify-center gap-9">
-        <h1 className="text-4xl font-bold text-white">{t("title")}</h1>
+      <div className="mx-auto text-center flex flex-col items-center justify-center gap-9">
+        <div className="relative h-[100vh] w-full">
+          <Image src={Mockup} alt="Mockup" width={400} height={400} className="object-cover absolute h-full w-full" />
+          <motion.div
+              className="absolute -translate-x-1/2 bottom-10 flex flex-col gap-5 text-left w-full container px-5 md:px-0 left-1/2"
+            >
+              <motion.h1
+                className="text-3xl font-bold text-white w-full uppercase"
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.8,
+                  ease: [0.25, 0.1, 0.25, 1],
+                  delay: 0,
+                }}
+              >
+                {t("title")}
+              </motion.h1>
 
+              <motion.p
+                initial={{ opacity: 0, y: 30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                  duration: 0.8,
+                  ease: [0.25, 0.1, 0.25, 1],
+                  delay: 0.2, // 👈 This creates the stagger effect
+                }}
+              >
+                SnapTogether is a simple way to collect photos and videos from your guests,
+                making every event more memorable — all without an app.
+              </motion.p>
+            </motion.div>
+        </div>
         <motion.div
           ref={containerRef}
           className="about-text flex flex-col items-center justify-center gap-12 text-white text-center"
