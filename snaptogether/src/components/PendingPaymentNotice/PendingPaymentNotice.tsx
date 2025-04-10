d662df18a13c43f1d3c32aa4b069f21de68959ea
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2025-04-10 13:46:39 +0200

    add copy button

--- snaptogether/src/components/PendingPaymentNotice/PendingPaymentNotice.tsx
+++ snaptogether/src/components/PendingPaymentNotice/PendingPaymentNotice.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { useTranslations } from "next-intl";
+import { Copy, Check } from "lucide-react"; // Optional icon library
+import { useState } from "react";
 
 interface PendingPaymentNoticeProps {
   plan: "starter" | "pro";
@@ -9,6 +11,20 @@ interface PendingPaymentNoticeProps {
 export default function PendingPaymentNotice({ plan }: PendingPaymentNoticeProps) {
   const t = useTranslations("eventForm.paymentInstructions");
 
+  const [copied, setCopied] = useState(false);
+
+  const accountNumber = t("accountNumber");
+
+    const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText('240197111431684');
+      setCopied(true);
+      setTimeout(() => setCopied(false), 2000);
+    } catch (err) {
+      console.error("❌ Failed to copy:", err);
+    }
+  };
+  
   return (
     <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 h-fit max-w-md w-full space-y-6 p-6 rounded-lg shadow-md bg-white opacity-90 text-center">
       <h2 className="text-xl font-bold text-gray-800">
@@ -19,8 +35,15 @@ export default function PendingPaymentNotice({ plan }: PendingPaymentNoticeProps
 
       <div className="bg-gray-100 rounded p-4 text-sm font-mono flex flex-col gap-4">
         <p className="text-slate-700">{t("bankName")}</p>
-        <p className="text-slate-700">{t("accountNumber")}</p>
-        {/* <p className="text-slate-700">{t("iban")}</p> */}
+        <div className="flex items-center justify-between p-2">
+          <span className="text-slate-700">{accountNumber} 240197111431684</span>
+          <button
+            onClick={handleCopy}
+            className="text-sm text-blue-600 hover:text-blue-800 transition"
+          >
+            {copied ? <Check size={16} /> : <Copy size={16} color="#1F2937" />}
+          </button>
+        </div>        {/* <p className="text-slate-700">{t("iban")}</p> */}
         <strong className="text-black">
           {plan === "starter" ? t("amountStarter") : t("amountPro")}
         </strong>
