dd1e7bccedca3dcbbfb82c1e9d003be603c962ff
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2025-04-10 12:01:32 +0200

    remove code from homepage

--- snaptogether/src/app/[locale]/page.tsx
+++ snaptogether/src/app/[locale]/page.tsx
@@ -48,16 +48,7 @@ const Home = () => {
   return (
     <div className="relative h-screen w-screen home-background">
       <Navbar />
-
-      {/* Background video & canvas overlay */}
-      <div className="video-container">
-        <video autoPlay muted loop playsInline>
-          <source src="/your-video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <canvas id="overlay-canvas"></canvas>
-      </div>
-
+      
       {/* Centered content */}
       <div className="absolute flex flex-col gap-4 items-center justify-center h-fit top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[91vw]">
         <motion.div
