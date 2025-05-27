71f2ff7c721bf897b2076b910c2a4e2f7d39d3d1
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2025-05-27 14:41:20 +0200

    default locale mk

--- snaptogether/src/i18n/routing.ts
+++ snaptogether/src/i18n/routing.ts
@@ -7,5 +7,4 @@ export const routing = defineRouting({
   // Used when no locale matches
   defaultLocale: 'mk',
   localeDetection: false, // ❌ disables browser-based redirection
-  localePrefix: 'as-needed' 
 });
