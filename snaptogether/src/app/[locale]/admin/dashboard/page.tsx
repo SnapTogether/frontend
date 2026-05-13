7ec3318a0e719687071e3cde4d3b5a133ffe539d
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2026-05-13 14:55:34 +0200

    fix login fetch

--- snaptogether/src/app/[locale]/admin/dashboard/page.tsx
+++ snaptogether/src/app/[locale]/admin/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useCallback, useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from '@/i18n/navigation';
 import { fetchAllEvents, approveEventPayment } from '@/api/admin';
 import { useLocale } from 'next-intl';
 
--- snaptogether/src/app/[locale]/admin/login/page.tsx
+++ snaptogether/src/app/[locale]/admin/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from '@/i18n/navigation';
 import { adminLogin } from '@/api/admin';
 
 export default function AdminLoginPage() {
