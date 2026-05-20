b4998f3f25c1832c90d1f429c40d1df64065f651
Stefan Gjorgjevski <stefan.gjorgjevski@yahoo.com>
2026-05-20 09:57:50 +0200

    fix build error

--- snaptogether/next.config.ts
+++ snaptogether/next.config.ts
@@ -2,6 +2,15 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/event/:path*',
+        destination: '/mk/event/:path*',
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
